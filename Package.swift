f73475d76ca99104ef1814433c0e47c2c62cab9f
Tomasz Kopycki <tomasz@thefabulous.co>
2025-09-21 09:12:30 +0200

    fix package

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "UIImageColors"
+    name: "UIImageColors",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        .library(name: "UIImageColors", targets: ["UIImageColors"])
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "UIImageColors", dependencies: [])
+    ]
 )
